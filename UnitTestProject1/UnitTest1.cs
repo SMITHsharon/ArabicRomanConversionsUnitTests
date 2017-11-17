bb3870b7073a1c9d2e999126af763653f84ce607
SMITHsharon <sharon.e.smith2@gmail.com>
2017-11-16 20:18:35 -0800

    test 1 => I set-up / passes

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumberConverter.Test
+{
+    [TestClass]
+    public class ArabicToRomanNumeralsTest
+    {
+        [TestMethod]
+        public void when_1_is_passed_I_should_be_returned()
+        {
+            //arrange
+            var converter = new RomanNumeralConverter();
+
+            //act
+            var result = converter.Convert(1);
+
+            //assert
+            Assert.AreEqual("I", result);
+        }
+    }
+}
